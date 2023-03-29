f94b3a61239a1cae932a8fcf80971fc90fc9e2c0
Ramesh kumar <42915529+ramesh998@users.noreply.github.com>
2023-03-29 21:29:10 +0530

    Update gallery_grid_view.dart

--- lib/src/presentation/widgets/gallery_grid/gallery_grid_view.dart
+++ lib/src/presentation/widgets/gallery_grid/gallery_grid_view.dart
@@ -133,7 +133,7 @@ class GalleryGridViewState extends State<GalleryGridView> {
                     );
                   }),
             ),
-          )
+          ))
         : Container();
   }
   
